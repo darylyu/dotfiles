9c506abb9f7b2ccc2aad5047f4b1437216ab16cf
D. Yu <dyu@fastmail.com>
2025-03-18 10:18:41 +0000

    quickstart.sh: symlinks for .zshenv

--- quickstart.sh
+++ quickstart.sh
@@ -21,6 +21,9 @@ ln -s -F $PWD/dyu.zsh-theme ~/.oh-my-zsh/themes/dyu.zsh-theme
 echo "Creating symlink ~/.zshrc"
 ln -s -F $PWD/zshrc ~/.zshrc
 
+echo "Creating symlink ~/.zshenv"
+ln -s -F $PWD/zshenv ~/.zshenv
+
 echo "Creating symlink ~/.config/i3/config"
 ln -s -F $PWD/i3_config ~/.config/i3/config
 
